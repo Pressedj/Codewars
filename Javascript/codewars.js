4a1bee60736792f2c26b5f7a9613c4d41b475394
Juniper <jpress.box@gmail.com>
2022-10-04 12:13:21 -0400

    8kyu

--- Javascript/codewars.js
+++ Javascript/codewars.js
@@ -546,3 +546,9 @@ decodeMorse = function (morseCode) {
     }
     return arr.join('').trim()
 }
+
+//8kyu Convert a Boolean to a String
+//link: https://www.codewars.com/kata/551b4501ac0447318f0009cd/train/javascript
+function booleanToString(b) {
+    return b.toString()
+}
